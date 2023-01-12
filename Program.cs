8dac01da8a6255d287561fd933d84b03dcbbb75c
esben laptop <esben9541@gmail.com>
2023-01-12 10:39:17 +0100

    Patch1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+
+namespace Dungeoncrawler
+
+{
+
+   internal class Program
+
+   {
+
+      static void Main(string[] args)
+
+      {
+
+         Console.WriteLine("Hello, You are in a dungeon");
+         Console.WriteLine("It is dark, but you can see 2 pathways");
+         Console.WriteLine("chose 1 or 2");
+         string pressed = Console.ReadKey().ToString();
+         Console.WriteLine("You died");
+
+
+      }
+
+   }
+
+}
