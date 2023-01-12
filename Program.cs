a6d3b6b2907ca56b242e8244caaaec2b86afb6a8
esben laptop <esben9541@gmail.com>
2023-01-12 10:48:00 +0100

    Sexd

--- Program.cs
+++ Program.cs
@@ -16,7 +16,14 @@ namespace Dungeoncrawler
          Console.WriteLine("It is dark, but you can see 2 pathways");
          Console.WriteLine("chose 1 or 2");
          string pressed = Console.ReadKey().ToString();
+         if (pressed != "1")
+    {
          Console.WriteLine("You died");
+    }
+        else
+        {
+             Console.WriteLine("You found the sex dungeon");
+        }
 
 
       }
